eea2e28f0a4df1f3b97080b67d75890e73f7fd5b
Jennifer Onyeama <61958046+AfricanJay@users.noreply.github.com>
2024-06-27 18:55:14 +0100

    updating...

---
+++ Exception Handling When Coding/Exception_Handling.py
@@ -0,0 +1,41 @@
+#Created By Jennifer Onyeama
+#Exceptional handling when coding
+#Exceptions handle errors in the code. They let you write contructs so that your program falls back to somewhere else if an error blocks the normal run of your code.
+
+class FormulaError(Exception): pass
+
+
+def parse_input(user_input):
+
+  input_list = user_input.split()
+  if len(input_list) != 3:
+    raise FormulaError('Input does not consist of three elements')
+  n1, op, n2 = input_list
+  try:
+    n1 = float(n1)
+    n2 = float(n2)
+  except ValueError:
+    raise FormulaError('The first and third input value must be numbers')
+  return n1, op, n2
+
+
+def calculate(n1, op, n2):
+
+  if op == '+':
+    return n1 + n2
+  if op == '-':
+    return n1 - n2
+  if op == '*':
+    return n1 * n2
+  if op == '/':
+    return n1 / n2
+  raise FormulaError('{0} is not a valid operator'.format(op))
+
+
+while True:
+  user_input = input('>>> ')
+  if user_input == 'quit':
+    break
+  n1, op, n2 = parse_input(user_input)
+  result = calculate(n1, op, n2)
+  print(result)
